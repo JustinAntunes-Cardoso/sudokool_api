da3d9b662a5e8a5c6cceca7882aef2d7e17267b2
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-08-07 12:48:30 -0400

    Hotfix for wsgi.py file.

--- sudokool_api/wsgi.py
+++ sudokool_api/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sudokool_api.settings")
 
 application = get_wsgi_application()
 app = application
